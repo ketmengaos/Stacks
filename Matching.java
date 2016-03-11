e5a891fa162136a4ddd345e3f23a7c3b5222386a
ketmengaos <ketmengaos@gmail.com>
2016-03-11 15:30:56 -0500

    Final Changes

--- Matching.java
+++ Matching.java
@@ -8,43 +8,54 @@ public class Matching {
     }
 
     public void valid() {
-        Stack stack = new Stack();
+        Stack<Character> stack = new Stack<Character>();
+
+	System.out.println(input);
 
         for (int i = 0; i < input.length(); i++) {
             char j = input.charAt(i);
+	    //System.out.println("Push J: " + j);  //-- Used this to test values.
             switch (j) {
                 case '<':
                 case '[':
                 case '{':
                 case '(':
                     stack.push(j);
+			//System.out.println("Push J: " + j); 
                     break;
                 case '>':
                 case ']':
                 case '}':
                 case ')':
                     if (!stack.isEmpty()) {
-                        Object k = stack.pop();
-                        if ((k.equals("<") && j == '>') ||
-                                (k.equals("[") && j == ']') ||
-                                (k.equals("{") && j == '}') ||
-                                (k.equals("(") && j == ')') ) {
-                            System.out.println("False 1.");
-                            System.exit(0);
-                        }
-                    }
+                        Character chara = (Character) stack.pop();
+			//System.out.println("Pop k = " + chara + j);  //	-- Used this to test values.
+                        if (j == '>' && chara != '<') {
+				System.out.println("False.");
+				System.exit(0);
+			}
+                        if (j == ']' && chara != '[') {
+				System.out.println("False.");
+				System.exit(0); 
+			}
+                        if (j == '}' && chara != '{') {
+				System.out.println("False.");
+				System.exit(0);
+			}
+                        if (j == ')' && chara != '(') {
+				System.out.println("False.");
+				System.exit(0); 
+			}
+		    }
                     else {
                         System.out.println("False.");
                         System.exit(0);
                     }
                     break;
                 default:
+			//System.out.println("Number: " + j);
                     break;
             }
-        }
-        if (!stack.isEmpty()) {
-            System.out.println("False.");
-            System.exit(0);
         }
     }
 }
--- Stacks.java
+++ Stacks.java
@@ -1,7 +1,15 @@
+//**********************************************************************************
+//******** || Ket-Meng Jimmy Cheng    ~    March 18, 2016    ~    Stacks || ********
+//******** || ---------------------------------------------------------- || ********
+//******** || This program takes an input String and returns true or     || ********
+//******** || false, depending on whether or not the String is properly  || ********
+//******** || formatted in terms of its delimiters.                      || ********
+//**********************************************************************************
+
 public class Stacks {
 
     public static void main(String[] args) {
-        String input = "[[]}{]";
+        String input = "{}{}{}{}{}[4]35<4366({})4>"; //Add input here
         Matching match = new Matching(input);
         match.valid();
         System.out.println("True.");
